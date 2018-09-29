e7944b03655dadc67058b9f6d77cc1e1c89623b6
97harsh <harsh25011997@hotmail.com>
2018-09-29 14:10:42 +0530

    Add files via upload

--- get_images.py
+++ get_images.py
@@ -10,36 +10,36 @@ import os
 # save the image
 
 def store_raw_image():
-    neg_image_link='http://www.image-net.org/api/text/imagenet.synset.geturls?wnid=n03906997'
+    neg_image_link='http://www.image-net.org/api/text/imagenet.synset.geturls?wnid=n04105893'
     #'http://www.image-net.org/api/text/imagenet.synset.geturls?wnid=n00450335'
     # http://www.image-net.org/api/text/imagenet.synset.geturls?wnid=n03906997 
     #pens
     #contains images of people 
     neg_image_urls=urllib.request.urlopen(neg_image_link).read().decode()
-    if not os.path.exists('Images_Pen/neg'):
-        os.makedirs('Images_Pen/neg')
+    if not os.path.exists('neg'):
+        os.makedirs('neg')
     
-    pic_num=890;
+    pic_num=1800;
     for i in neg_image_urls.split('\n'):
         try:
             print(i)
-            urllib.request.urlretrieve(i,'Images_Pen/neg/{}.jpg'.format(pic_num))
-            img=cv2.imread('Images_Pen/neg/{}.jpg'.format(pic_num),cv2.IMREAD_GRAYSCALE)
+            urllib.request.urlretrieve(i,'neg/{}.jpg'.format(pic_num))
+            img=cv2.imread('neg/{}.jpg'.format(pic_num),cv2.IMREAD_GRAYSCALE)
             resized_image= cv2.resize(img,(100,100))
-            cv2.imwrite('Images_Pen/neg/{}.jpg'.format(pic_num),resized_image)
+            cv2.imwrite('neg/{}.jpg'.format(pic_num),resized_image)
             pic_num+=1;
 
         except Exception as e:
             print(str(e))
 
 def find_nonsense(): # removes images similar to images placed in nonsense folder
-    for file_type in ['Images_Pen/neg']:
+    for file_type in ['neg']:
         for img in os.listdir(file_type):
-            for nonsense in os.listdir('Images_Pen/Nonsense'):
+            for nonsense in os.listdir('Nonsense'):
                 try:
                     image_path=str(file_type)+'/'+str(img)
                     # print(image_path)
-                    useless=cv2.imread('Images_Pen/Nonsense'+'/'+str(nonsense))
+                    useless=cv2.imread('Nonsense'+'/'+str(nonsense))
                     # print(useless.shape)
                     check=cv2.imread(image_path)
                     # print(check.shape)
@@ -53,11 +53,11 @@ def find_nonsense(): # removes images similar to images placed in nonsense folde
 
 
 def pos_n_neg():
-    for file_type in ['Images_Pen/neg']:
+    for file_type in ['neg']:
         for img in os.listdir(file_type):
-            if file_type=='Images_Pen/neg':
+            if file_type=='neg':
                 line='./'+file_type+'/'+img+'\n\n'
-                with open('Images_Pen/bg.txt','a') as f: #all files containing negative images
+                with open('bg.txt','a') as f: #all files containing negative images
                     f.write(line)
             # elif file_type=='Images_Pen/pos':
             #     line=file_type+'/'+img+' 1 0 0 50 50\n' #expecting image to be present in 50*50 box
@@ -65,4 +65,6 @@ def pos_n_neg():
             #     with open('Images_Pen/info.txt','a') as f:
             #         f.write(line)
 
+# store_raw_image()
+# find_nonsense()
 pos_n_neg()
